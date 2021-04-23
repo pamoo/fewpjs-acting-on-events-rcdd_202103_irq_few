7530469d5849a3e78f43ce0d7c7295e240a88c52
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-23 20:21:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#000000";
 dodger.style.backgroundColor = "#FF69B4";
+console.log(dodger.style.left); // "180px"
+console.log(dodger.style.bottom); // "0px"
